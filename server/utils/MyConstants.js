0d3579e6ea01e355c7354982e913b42c4434d8df
khaiblack123 <nguyenphanchikhaiphuyen@gmail.com>
2024-07-17 16:19:36 +0700

    first commit

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.e2op7ck.mongodb.net',
+    DB_USER: 'khaiblack2210',
+    DB_PASS: 'khaiblack2210',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: '2210',
+    JWT_EXPIRES: '22222222222222222222222', // in milliseconds
+    EMAIL_USER: 'nguyenphanchikhaiphuyen@gmail.com', // gmail service
+    EMAIL_PASS: 'vkmlzomdbxzjwdui'
+  };
+  module.exports = MyConstants;
